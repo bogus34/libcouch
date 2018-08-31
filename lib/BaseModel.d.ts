9908d47fe0a93b20da5ed4ddf1bbfe89629bc7a4
Alexander Bogdanov <andorn@gmail.com>
2018-08-31 17:19:46 +0300

    v0.0.3 - some tests and fixes for models

--- lib/BaseModel.d.ts
+++ lib/BaseModel.d.ts
@@ -9,21 +9,27 @@ export declare abstract class BaseStore<M extends BaseModelInterface, D extends 
     protected Model: ModelConstructor<M>;
     toId(instanceId: any): ID;
     makeId(instanceId: any): ID;
-    toInstanceId(id: ID): string | number;
+    toInstanceId(id: ID | number): string | number;
     create(data: D): M;
     find(id: any): Promise<M | null>;
     findOr404(id: any): Promise<M>;
     save(instance: M): Promise<M>;
     bulkRemove(docs: Document[]): Promise<any>;
     reload(model: M): Promise<M>;
-    modify(instance: M, fn: (instance: M) => M): Promise<M>;
+    modify(instance: M, fn: (instance: M) => M | null): Promise<M>;
     remove(instance: M): Promise<void>;
 }
-export declare class BaseModel implements BaseModelInterface {
+export declare abstract class BaseModel implements BaseModelInterface {
     id: string;
     instanceId: string | number;
     _rev?: string;
     $type: string;
     $created?: number;
     $updated?: number;
+}
+export declare abstract class BaseMapper implements MapperInterface<BaseModel, BaseDoc> {
+    prefix: string;
+    type: string;
+    loadJSON(model: BaseModel, data: any): void;
+    dumpJSON(model: BaseModel): BaseDoc;
 }
